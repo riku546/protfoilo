3b093f8fbf1ed7818a2bc30c70f9b16235d28152
riku546 <91833521+riku546@users.noreply.github.com>
2022-07-10 15:53:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
-// var scrollAnimationElm = document.querySelectorAll('.fadeIn');
-// var scrollAnimationFunc = function() {
-//   for(var i = 0; i < scrollAnimationElm.length; i++) {
-//     var triggerMargin = 300;
-//     if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-//       scrollAnimationElm[i].classList.add('show');
-//     }
-//   }
-// }
-// window.addEventListener('load', scrollAnimationFunc);
-// window.addEventListener('scroll', scrollAnimationFunc);
+var scrollAnimationElm = document.querySelectorAll('.fadeIn');
+var scrollAnimationFunc = function() {
+  for(var i = 0; i < scrollAnimationElm.length; i++) {
+    var triggerMargin = 300;
+    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+      scrollAnimationElm[i].classList.add('show');
+    }
+  }
+}
+window.addEventListener('load', scrollAnimationFunc);
+ window.addEventListener('scroll', scrollAnimationFunc);
 
 const hamburger_menu=document.querySelector(".hamburger-menu");
 const header_list=document.querySelector(".header-list");
